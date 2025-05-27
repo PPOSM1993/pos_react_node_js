8b63bf4a60f08f4a92a031a00902b8cdfa4cb44c
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-05-26 22:02:11 -0400

    Se inicia el Proyecto de Point of Sale con React y Node JS, inicio dahsboard

---
+++ pos-frontend/src/index.js
@@ -0,0 +1,7 @@
+export { default as App } from './App';
+export { default as BottomNav } from './components/shared/BottomNav';
+export { default as Header } from './components/shared/Header';
+export * from './main';
+export { default as Auth } from './pages/Auth';
+export { default as Home } from './pages/Home';
+export { default as Orders } from './pages/Orders';
